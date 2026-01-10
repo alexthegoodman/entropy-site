bbb0a5291dd141a87535b9fdaecc2835d73c9d94
Alex Woodman <alexthegoodman@gmail.com>
2026-01-10 02:00:33 -0500

    fixes

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,16 @@
-import { PrismaClient } from '@prisma/client'
+import "dotenv/config";
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
+
+const connectionString = `${process.env.DATABASE_URL}`
+
+const adapter = new PrismaPg({ connectionString })
+// const prisma = new PrismaClient({ adapter })
+
+// export { prisma }
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  return new PrismaClient({ adapter })
 }
 
 declare global {
