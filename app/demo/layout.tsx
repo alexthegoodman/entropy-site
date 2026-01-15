17a2307ffad56470bc0fcd267200daf2e4e6b58a
Alex Woodman <alexthegoodman@gmail.com>
2026-01-14 20:39:53 -0500

    fix demo

--- app/demo/layout.tsx
+++ app/demo/layout.tsx
@@ -1,11 +1,18 @@
 import Head from "next/head";
 
-export default function Layout() {
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  other: {
+    modulepreload: "https://stunts.b-cdn.net/entropy-chat-d94bd7e2ee5724d1.js",
+    preload: "https://stunts.b-cdn.net/entropy-chat-d94bd7e2ee5724d1_bg.wasm",
+  }
+};
+
+export default function Layout({ children = null }) {
     return (
         <>
-            <Head>
-                <link rel="modulepreload" href="https://stunts.b-cdn.net/entropy-chat-d94bd7e2ee5724d1.js" crossOrigin="anonymous" integrity="sha384-IPf0Pk2uPVJGlzs6ehER77hdfGvz6KsFtH0Qn0uE7LNC/AqDmKIF0dx+wXTEWcMx"/><link rel="preload" href="https://stunts.b-cdn.net/entropy-chat-d94bd7e2ee5724d1_bg.wasm" crossOrigin="anonymous" integrity="sha384-3HsUuNZOoSd5WOAh+6GyzYRIQAx3D+hQJOWYIbFn3k/6slxOVgg0wQB0JhDiRayI" as="fetch" type="application/wasm"/>
-            </Head>
+            {children}
         </>
     )
 }
